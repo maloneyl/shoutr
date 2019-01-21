3301c23e62985d6a6639844d34d48ceea7ed98ba
Maloney L <maloneyl@users.noreply.github.com>
2019-01-21 19:41:52 +0000

    add dependency injection for Timeline more reusable
    - make Shout the default scope
    - possible to pass in shouts that match certain criteria

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,10 +1,11 @@
 class Timeline
-  def initialize(users)
+  def initialize(users, scope = Shout)
     @users = users
+    @scope = scope
   end
 
   def shouts
-    Shout.
+    scope.
       where(user_id: users).
       order(created_at: :desc)
   end
@@ -16,5 +17,5 @@ class Timeline
 
   private
 
-  attr_reader :users
+  attr_reader :users, :scope
 end
