761990a3b24d5a078a9940c2ddaabed434e096e9
Maloney L <maloneyl@users.noreply.github.com>
2019-01-22 16:25:07 +0000

    use Solr with Sunspot to perform search instead

--- Gemfile
+++ Gemfile
@@ -38,10 +38,12 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'clearance'
 gem 'paperclip'
+gem 'sunspot_rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'sunspot_solr'
 end
 
 group :development do
--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -1,33 +1,14 @@
 class ShoutSearchQuery
-  def initialize(term:, scope: Shout.all)
+  def initialize(term:)
     @term = term
-    @scope = scope
   end
 
   def to_relation
-    matching_shouts_for_text_shouts.
-      or(matching_shouts_for_photo_shouts)
+    hits = Sunspot.search([TextShout, PhotoShout]) { fulltext term }.hits
+    Shout.where(content: hits.map { |hit| hit.class_name.constantize.new(id: hit.primary_key) })
   end
 
   private
 
-  attr_reader :term, :scope
-
-  def matching_shouts_for_text_shouts
-    scope.
-      where(content_type: "TextShout", content_id: matching_text_shouts.select(:id))
-  end
-
-  def matching_text_shouts
-    TextShout.where("body LIKE ?", "%#{term}%")
-  end
-
-  def matching_shouts_for_photo_shouts
-    scope.
-      where(content_type: "PhotoShout", content_id: matching_photo_shouts.select(:id))
-  end
-
-  def matching_photo_shouts
-    PhotoShout.where("image_file_name LIKE ?", "%#{term}%")
-  end
+  attr_reader :term
 end
